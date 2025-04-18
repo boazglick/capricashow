01dbe570600b3b49570e50c5269ff9ff07325a74
spidernet <95533522+boazglick@users.noreply.github.com>
2025-04-18 12:44:58 +0300

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,11 @@
 // gatsby-ssr.js
 import React from "react";
 
-export const onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+  // קובע את כיוון האתר והגדרת שפה
+  setHtmlAttributes({ lang: "he", dir: "rtl" });
+
+  // ממשיך עם הקוד שכבר היה לך
   setHeadComponents([
     <meta
       key="google-adsense-account"
@@ -12,7 +16,7 @@ export const onRenderBody = ({ setHeadComponents }) => {
       key="google-adsense"
       async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8148276210846697"
-      crossorigin="anonymous"
-    />
+      crossOrigin="anonymous"
+    />,
   ]);
 };
